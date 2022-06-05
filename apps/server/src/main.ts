d963c67348fcb7d31853d87317ed9503d97a1c07
Pavel Tyukin <tyukin.pavel@yandex.ru>
2022-06-05 08:41:16 +0300

    feat: add config

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,8 +1,14 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+const PORT = process.env.PORT
+const HOST = process.env.HOST
+
+async function start() {
+  const app = await NestFactory.create(AppModule)
+  await app.listen(PORT, HOST, () =>
+    console.log(`Serve started on HOST=${HOST} PORT=${PORT}`)
+  )
 }
-bootstrap();
+
+start().catch((err) => `Server error = ${err}`)
